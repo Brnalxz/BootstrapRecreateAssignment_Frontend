0154704e8dc5b1af10f5918b08c0f6f7df0df52c
Brnalxz <alexander.brandon425@gmail.com>
2024-10-28 20:19:19 +0700

    Brandon_535230088

---
+++ script.js
@@ -0,0 +1,9 @@
+function setActive(event) {
+    document.querySelectorAll('.nav-link').forEach(link => {
+        link.classList.remove('active');
+    });
+ 
+    event.currentTarget.classList.add('active');
+}
+
+
